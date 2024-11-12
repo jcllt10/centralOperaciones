f3378f500b2e8c9905f04682633b0c0e800ffa11
jcllt10 <llaczajc10@gmail.com>
2024-11-11 21:18:51 -0500

    primer commit resultado-service

---
+++ resultado-service/src/main/java/com/microservicio/resultado/controller/ResultadoController.java
@@ -0,0 +1,16 @@
+package com.microservicio.resultado.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResultadoController {
+    
+    @GetMapping("/operar/{resultado}")
+    public String mostrarResultado(@PathVariable float resultado)
+    {
+        return "El resultado es : " + resultado;
+     }
+    
+}
---
+++ resultado-service/src/test/java/com/microservicio/resultado/ResultadoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.resultado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultadoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
